f9ed04e33c823d1d0ff266829a1f32d1d8902fb4
Tadesse Zemicheal <tadesse.habte@gmail.com>
2015-10-21 12:19:43 -0700

    Update utility.hpp

--- Knapsack/util/utility.hpp
+++ Knapsack/util/utility.hpp
@@ -9,11 +9,13 @@ namespace util {
         template<class T>
         T max(T a, T b) { return (a > b ? a : b); }
         template<class T>
+        T min(T a, T b) {return (a<b?a:b);}
+        template<class T>
         T mean(std::vector<T> values)
-        {       T sum =0.0;
+        {       double sum =0.0;
                 for(T el : values)
                         sum +=el;
-                return sum/values.size();
+                return sum/(double)values.size();
         }
 }
 
